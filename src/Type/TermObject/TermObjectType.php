6ec5f1ebfb03dbe14c1e1d309305623ebd718ce8
jasonbahl <jasonbahl@mac.com>
2017-03-17 09:00:06 -0600

    WPGraphQL #52 - PostType to Taxonomies connections

--- src/Type/TermObject/TermObjectType.php
+++ src/Type/TermObject/TermObjectType.php
@@ -4,7 +4,6 @@ namespace WPGraphQL\Type\TermObject;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQLRelay\Relay;
 use WPGraphQL\Type\PostObject\Connection\PostObjectConnectionDefinition;
-use WPGraphQL\Type\TermObject\Connection\TermObjectConnectionDefinition;
 use WPGraphQL\Type\WPObjectType;
 use WPGraphQL\Types;
 
