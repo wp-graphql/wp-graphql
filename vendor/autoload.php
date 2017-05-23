4ec34c95c61ca7e5d12a13da20de9234d1d8819f
jasonbahl <jasonbahl@mac.com>
2017-05-23 14:51:34 -0600

    Update composer files to include new Autoloading classes for mutations

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitcc440e3b13871dd8fcc9048ccceadfef::getLoader();
+return ComposerAutoloaderInit1e072e261675e0b8440f1da532408811::getLoader();
