675c1499b7a3dd37f0619acc86a0b799b0976e57
jasonbahl <jasonbahl@mac.com>
2017-07-12 08:10:40 -0600

    #78 - test router
    Add tests for theme queries

---
+++ tests/test-theme-queries.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * WPGraphQL Test Theme Queries
+ * @package WPGraphQL
+ */
+class WP_GraphQL_Test_Theme_Queries extends WP_UnitTestCase {
+
+	public $current_time;
+	public $current_date;
+	public $current_date_gmt;
+	public $admin;
+
+
+	/**
+	 * This function is run before each method
+	 * @since 0.0.5
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->current_time = strtotime( 'now' );
+		$this->current_date = date( 'Y-m-d H:i:s', $this->current_time );
+		$this->current_date_gmt = gmdate( 'Y-m-d H:i:s', $this->current_time );
+		$this->admin = $this->factory->user->create( [
+			'role' => 'administrator',
+		] );
+
+	}
+
+	/**
+	 * Runs after each method.
+	 * @since 0.0.5
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * testThemesQuery
+	 * This tests querying for themes to ensure that we're getting back a proper connection
+	 */
+	public function testThemesQuery() {
+
+		$query = '
+		{
+		  themes(last: 1){
+		    edges{
+		      node{
+		        id
+		        name
+		      }
+		    }
+		  }
+		}
+		';
+
+		$actual = do_graphql_request( $query );
+
+		/**
+		 * We don't really care what the specifics are, we just want to make sure we are
+		 * properly getting a theme back in the query
+		 */
+		$this->assertNotEmpty( $actual['data']['themes']['edges'] );
+		$this->assertNotEmpty( $actual['data']['themes']['edges'][0]['node']['id'] );
+		$this->assertNotEmpty( $actual['data']['themes']['edges'][0]['node']['name'] );
+		$this->assertEquals( $actual['data']['themes']['edges'][0]['node']['id'], 'dGhlbWU6dHdlbnR5c2l4dGVlbg==' );
+
+	}
+
+	/**
+	 * testThemeQuery
+	 * This queries for a specific theme based on the ID
+	 */
+	public function testThemeQuery() {
+
+		$query = '
+		{
+		  theme(id: "dGhlbWU6dHdlbnR5c2l4dGVlbg=="){
+		    id
+		    name
+		  }
+		}
+		';
+
+		$actual = do_graphql_request( $query );
+
+		$expected = [
+			'data' => [
+				'theme' => [
+					'id' => 'dGhlbWU6dHdlbnR5c2l4dGVlbg==',
+					'name' => 'Twenty Sixteen',
+				],
+			],
+		];
+
+		$this->assertEquals( $expected, $actual );
+
+	}
+
+}
