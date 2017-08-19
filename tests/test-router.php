1109cc55ed78b6c1e0571e2e8e94ac881d07686e
Aram Zucker-Scharff <aramzs@salon.com>
2017-08-19 15:45:57 -0600

    Tests for empty router request and expected JSON of empty router requests
    These tests should cover #200

--- tests/test-router.php
+++ tests/test-router.php
@@ -49,6 +49,7 @@ class WPGraphQL_Test_Router extends WP_UnitTestCase {
 
 	/**
 	 * Test the "send_header" method in the Router class
+	 *
 	 * @see: https://github.com/sebastianbergmann/phpunit/issues/720
 	 * @runInSeparateProcess
 	 */
@@ -123,7 +124,9 @@ class WPGraphQL_Test_Router extends WP_UnitTestCase {
 		 */
 		add_filter( 'graphql_request_data', function( $data ) {
 			$data['query'] = 'query getPosts($first:Int){ posts(first:$first){ edges{ node{ id } } } }';
-			$data['variables'] = [ 'first' => 1 ];
+			$data['variables'] = [
+				'first' => 1,
+			];
 			$data['operationName'] = 'getPosts';
 			return $data;
 		} );
@@ -173,7 +176,9 @@ class WPGraphQL_Test_Router extends WP_UnitTestCase {
 		 */
 		add_filter( 'graphql_request_data', function( $data ) {
 			$data['query'] = 'query getPosts($first:Int){ posts(first:$first){ edges{ node{ id } } } }';
-			$data['variables'] = wp_json_encode( [ 'first' => 1 ] );
+			$data['variables'] = wp_json_encode( [
+				'first' => 1,
+			] );
 			$data['operationName'] = 'getPosts';
 			return $data;
 		} );
@@ -227,6 +232,48 @@ class WPGraphQL_Test_Router extends WP_UnitTestCase {
 		 * Process the request using our filtered data
 		 */
 		$this->assertNull( $router::resolve_http_request() );
+
+	}
+
+	public function testResolveHttpRequestWithEmptyQuery() {
+
+		/**
+		 * Filter the request data
+		 */
+		add_filter( 'graphql_request_data', function( $data ) {
+			$data['query'] = null;
+			$data['variables'] = null;
+			$data['operationName'] = null;
+			return $data;
+		} );
+
+		/**
+		 * Set the query var to "graphql" so we can mock like we're visiting the endpoint via
+		 */
+		set_query_var( 'graphql', true );
+		$GLOBALS['wp']->query_vars['graphql'] = true;
+
+		/**
+		 * Instantiate the router
+		 */
+		$router = new \WPGraphQL\Router();
+
+		/**
+		 * Process the request using our filtered data
+		 */
+		$router::resolve_http_request();
+
+		/**
+		 * Make sure the constant gets defined when it's a GraphQL Request
+		 */
+		$this->assertTrue( defined( 'GRAPHQL_HTTP_REQUEST' ) );
+		$this->assertEquals( true, GRAPHQL_HTTP_REQUEST );
+
+		/**
+		 * Make sure the actions we expect to be firing are firing
+		 */
+		$this->assertNotFalse( did_action( 'graphql_process_http_request' ) );
+		$this->assertNotFalse( did_action( 'graphql_process_http_request_response' ) );
 
 	}
 
