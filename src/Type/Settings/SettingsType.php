a29603c8e84875c8f50605e7f70cf5a4fc7c0699
hughdevore <hughdevore@gmail.com>
2017-10-05 09:52:57 -0600

    #156 Initial commit.

---
+++ src/Type/Settings/SettingsType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WPGraphQL\Type\Settings;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use WPGraphQL\Types;
+
+/**
+ * Class SettingsType
+ *
+ * This class is a full definition of a new type
+ * called Settings, which will contain
+ */
