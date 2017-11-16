898ecea2dccecb90151d069752b9360629f33867
Jason <jasonbahl@mac.com>
2017-11-15 22:15:18 -0700

    no message

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,16 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'cli' => array($vendorDir . '/wp-cli/php-cli-tools/lib'),
-    'WP_CLI' => array($vendorDir . '/wp-cli/wp-cli/php'),
-    'Requests' => array($vendorDir . '/rmccue/requests/library'),
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
-    'Oxymel' => array($vendorDir . '/nb/oxymel'),
-    'Mustache' => array($vendorDir . '/mustache/mustache/src'),
-    'Hautelook' => array($vendorDir . '/hautelook/phpass/src'),
-    'Handlebars' => array($vendorDir . '/xamin/handlebars.php/src'),
-    'Guzzle\\Tests' => array($vendorDir . '/guzzle/guzzle/tests'),
-    'Guzzle' => array($vendorDir . '/guzzle/guzzle/src'),
-    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
-    'BaconStringUtils' => array($vendorDir . '/bacon/bacon-string-utils/src'),
 );
