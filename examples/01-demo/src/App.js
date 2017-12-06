b61d68fc8bb6fabb4e1d6443f6848d48a55e344d
Jason <jasonbahl@mac.com>
2017-12-06 12:26:30 -0700

    - testing heroku

--- examples/01-demo/src/App.js
+++ examples/01-demo/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Heroku Rox!</h1>
+          <h1 className="App-title">Heroku Rox! This is an automated deploy...</h1>
         </header>
       </div>
     );
