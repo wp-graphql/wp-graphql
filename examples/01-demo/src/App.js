00001450b39b1b8ac80e1c9b428f0b2799152d91
Jason <jasonbahl@mac.com>
2017-12-06 12:25:11 -0700

    - testing heroku

--- examples/01-demo/src/App.js
+++ examples/01-demo/src/App.js
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Heroku Rox!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
