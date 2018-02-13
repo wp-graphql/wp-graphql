8f67281157e94df0bdba2f519bcd0d1b1175d268
Jason <jasonbahl@mac.com>
2018-02-13 14:46:53 -0700

    no message

--- src/Type/PostObject/Mutation/PostObjectCreate.php
+++ src/Type/PostObject/Mutation/PostObjectCreate.php
@@ -91,6 +91,31 @@ class PostObjectCreate {
 					$post_args = PostObjectMutation::prepare_post_object( $input, $post_type_object, $mutation_name );
 
 					/**
+					 * Filter the default post status to use when the post is initially created. Pass through a filter to
+					 * allow other plugins to override the default (for example, Edit Flow, which provides control over
+					 * customizing stati or various E-commerce plugins that make heavy use of custom stati)
+					 *
+					 * @param string $default_status The default status to be used when the post is initially inserted
+					 * @param \WP_Post_Type $post_type_object The Post Type that is being inserted
+					 * @param string $mutation_name The name of the mutation currently in progress
+					 */
+					$default_post_status = apply_filters( 'graphql_post_object_create_default_post_status', 'draft', $post_type_object, $mutation_name );
+
+					/**
+					 * We want to cache the "post_status" and set the status later. We will set the initial status
+					 * of the inserted post as the default status for the site, allow side effects to process with the
+					 * inserted post (set term object connections, set meta input, sideload images if necessary, etc)
+					 * Then will follow up with setting the status as what it was declared to be later
+					 */
+					$intended_post_status = ! empty( $post_args['post_status'] ) ? $post_args['post_status'] : $default_post_status;
+
+					/**
+					 * Set the post_status as the default for the initial insert. The intended $post_status will be set after
+					 * side effects are complete.
+					 */
+					$post_args['post_status'] = $default_post_status;
+
+					/**
 					 * Insert the post and retrieve the ID
 					 */
 					$post_id = wp_insert_post( wp_slash( (array) $post_args ), true );
@@ -121,6 +146,43 @@ class PostObjectCreate {
 					 * postObject that was created so that relations can be set, meta can be updated, etc.
 					 */
 					PostObjectMutation::update_additional_post_object_data( $post_id, $input, $post_type_object, $mutation_name );
+
+					/**
+					 * Determine whether the intended status should be set or not.
+					 *
+					 * By filtering to false, the $intended_post_status will not be set at the completion of the mutation.
+					 *
+					 * This allows for side-effect actions to set the status later. For example, if a post
+					 * was being created via a GraphQL Mutation, the post had additional required assets, such as images
+					 * that needed to be sideloaded or some other semi-time-consuming side effect, those actions could
+					 * be deferred (cron or whatever), and when those actions complete they could come back and set
+					 * the $intended_status.
+					 *
+					 * @param boolean $should_set_intended_status Whether to set the intended post_status or not. Default true.
+					 * @param \WP_Post_Type $post_type_object The Post Type Object for the post being mutated
+					 * @param string $mutation_name The name of the mutation currently in progress
+					 * @param string $intended_post_status The intended post_status the post should have according to the mutation input
+					 * @param string $default_post_status The default status posts should use if an intended status wasn't set
+					 */
+					$should_set_intended_status = apply_filters( 'graphql_post_object_create_should_set_intended_post_status', true, $post_type_object, $mutation_name, $intended_post_status, $default_post_status  );
+
+					/**
+					 * If the intended post status and the default post status are not the same,
+					 * update the post with the intended status now that side effects are complete.
+					 */
+					if ( $intended_post_status !== $default_post_status && true === $should_set_intended_status ) {
+
+						$update_args = [
+							'ID' => $post_id,
+							'post_status' => $intended_post_status,
+							// Prevent the post_date from being reset if the date was included in the create post $args
+							// see: https://core.trac.wordpress.org/browser/tags/4.9/src/wp-includes/post.php#L3637
+							'edit_date' => ! empty( $post_args['post_date'] ) ? $post_args['post_date'] : false,
+						];
+
+						$update_args = array_merge( $update_args );
+						wp_update_post( $update_args );
+					}
 
 					/**
 					 * Return the post object
