cc2d051f54e4fd91f50a28bc3d7dfa4de81d7ace
Geoff Taylor <geoffrey.taylor@outlook.com>
2018-11-04 18:05:05 -0500

    ThemeMods-related types and mutations refactored and added. ThemeModsQueriesTest refactored

---
+++ src/Type/Input/CustomBackgroundInput.php
@@ -0,0 +1,28 @@
+<?php
+namespace WPGraphQL\Type;
+
+register_graphql_input_type( 'CustomBackgroundInput', [
+	'description' => __( 'Custom background values' ),
+	'fields' => [
+		'imageId' => [
+			'type'        => 'ID',
+			'description' => __( 'The theme mod "background"\'s image attachment id', 'wp-graphql' ),
+		],
+		'preset' => [
+			'type'        => 'String',
+			'description' => __( 'The theme mod "background"\'s preset property', 'wp-graphql' ),
+		],
+		'size' => [
+			'type'        => 'String',
+			'description' => __( 'The theme mod "background"\'s css background-size property', 'wp-graphql' ),
+		],
+		'repeat' => [
+			'type'        => 'String',
+			'description' => __( 'The theme mod "background"\'s css background-repeat property', 'wp-graphql' ),
+		],
+		'attachment' => [
+			'type'        => 'String',
+			'description' => __( 'The theme mod "background"\'s css background-attachement property', 'wp-graphql' ),
+		],
+	],
+] );
