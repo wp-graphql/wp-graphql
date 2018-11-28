b4819171faa30f5341841c45993011c6bf3a5e5c
chriszarate <chris@zarate.org>
2018-11-28 00:28:01 -0500

    Restore graphql_input_fields filter.
    The filter was not firing after the type registry changes because the
    fields definition was wrapped in a closure and could only be modified
    or filtered inside the closure.

--- src/Type/WPInputObjectType.php
+++ src/Type/WPInputObjectType.php
@@ -15,18 +15,6 @@ use GraphQL\Type\Definition\InputObjectType;
 class WPInputObjectType extends InputObjectType {
 
 	/**
-	 * WPInputObjectType constructor.
-	 *
-	 * @param array $config The configuration for the InputObjectType
-	 */
-	public function __construct( $config = [] ) {
-		if ( ! empty( $config['fields'] ) && is_array( $config['fields'] ) ) {
-			$config['fields'] = self::prepare_fields( $config['fields'], $config['name'], $config );
-		}
-		parent::__construct( $config );
-	}
-
-	/**
 	 * prepare_fields
 	 *
 	 * This function sorts the fields and applies a filter to allow for easily
@@ -52,17 +40,6 @@ class WPInputObjectType extends InputObjectType {
 		$fields = apply_filters( 'graphql_input_fields', $fields, $type_name, $config );
 
 		/**
-		 * Pass the fields through a filter
-		 *
-		 * @param array $fields
-		 * @param string $type_name
-		 * @param array $config
-		 * @since 0.0.5
-		 */
-		$fields = apply_filters( 'graphql_' . lcfirst( $type_name ) . '_fields', $fields, $type_name, $config );
-		$fields = apply_filters( "graphql_{$type_name}_fields", $fields, $type_name, $config );
-
-		/**
 		 * Sort the fields alphabetically by key. This makes reading through docs much easier
 		 * @since 0.0.2
 		 */
@@ -74,18 +51,4 @@ class WPInputObjectType extends InputObjectType {
 		 */
 		return $fields;
 	}
-
-	/**
-	 * format_enum_name
-	 *
-	 * This formats enum_names to be all caps with underscores for spaces/word-breaks
-	 *
-	 * @param $name
-	 * @return string
-	 * @since 0.0.5
-	 */
-	public static function format_enum_name( $name ) {
-		return strtoupper( preg_replace( '/[^A-Za-z0-9]/i', '_', $name ) );
-	}
-
 }
