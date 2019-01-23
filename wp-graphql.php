6c196f847ad6e42be4c18c56346686078c8fb2b5
Jason Bahl <jasonbahl@mac.com>
2019-01-23 10:09:39 -0700

    🚀 RELEASE: Updating version numbers to prepare for release

--- wp-graphql.php
+++ wp-graphql.php
@@ -5,7 +5,7 @@
  * Description: GraphQL API for WordPress
  * Author: WPGraphQL
  * Author URI: http://www.wpgraphql.com
- * Version: 0.2.0
+ * Version: 0.2.1
  * Text Domain: wp-graphql
  * Domain Path: /languages/
  * Requires at least: 4.7.0
@@ -18,7 +18,7 @@
  * @package  WPGraphQL
  * @category Core
  * @author   WPGraphQL
- * @version  0.2.0
+ * @version  0.2.1
  */
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -167,7 +167,7 @@ if ( ! class_exists( 'WPGraphQL' ) ) :
 
 			// Plugin version.
 			if ( ! defined( 'WPGRAPHQL_VERSION' ) ) {
-				define( 'WPGRAPHQL_VERSION', '0.2.0' );
+				define( 'WPGRAPHQL_VERSION', '0.2.1' );
 			}
 
 			// Plugin Folder Path.
