fe703e4f5c9443baf2bb933a1807162b982f853c
Jason Bahl <jasonbahl@mac.com>
2019-02-06 16:08:26 -0700

    📦 UPDATE COMPOSER FILES
    #604

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5f22c2584a38578ef9509229a7fddb30::getLoader();
+return ComposerAutoloaderInit5861927d8ec2d38ee25dd07b7fdfd61a::getLoader();
