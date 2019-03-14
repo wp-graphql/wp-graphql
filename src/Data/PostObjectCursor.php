010bb206ff911db6c8ea9d8863c843522755ce0c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-03-14 22:08:48 +0200

    Add some code comments

--- src/Data/PostObjectCursor.php
+++ src/Data/PostObjectCursor.php
@@ -67,9 +67,13 @@ class PostObjectCursor {
 	}
 
 	/**
-	 * Return the additional AND operations for the where statement
+	 * Return the additional AND operators for the where statement
 	 */
 	public function get_where() {
+
+		/**
+		 * If we have no cursor just compare with the ids
+		 */
 		if ( ! $this->cursor_post ) {
 			return $this->compare_with_id();
 		}
@@ -77,22 +81,33 @@ class PostObjectCursor {
 		$orderby = $this->query->get( 'orderby' );
 
 		if ( ! empty( $orderby ) && is_array( $orderby ) ) {
-			$where = ' ';
+			/**
+			 * Loop through all order keys if it is an array
+			 */
+			$where = '';
 			foreach ( $orderby as $by => $order ) {
 				$where .= $this->compare_with( $by, $order );
 			}
 			return $where;
 		} else if ( ! empty( $orderby ) && is_string( $orderby ) ) {
+			/**
+			 * If $orderby is just a string just compare with it directly
+			 */
 			$order = ! empty( $this->query->query_vars['order'] ) ? $this->query->query_vars['order'] : 'DESC' ;
 			return $this->compare_with( $orderby, $order );
 		}
 
+		/**
+		 * Default to comparing by ids if no ordering is set
+		 */
 		return $this->compare_with_id();
 
 	}
 
 	/**
 	 * Get AND operator for ID based comparison
+	 *
+	 * @return string
 	 */
 	private function compare_with_id() {
 		return $this->wpdb->prepare( " AND {$this->wpdb->posts}.ID {$this->compare} %d", $this->cursor_offset );
@@ -100,6 +115,8 @@ class PostObjectCursor {
 
 	/**
 	 * Get AND operator for post date based comparison
+	 *
+	 * @return string
 	 */
 	private function compare_with_date() {
 		return $this->wpdb->prepare(
@@ -110,7 +127,7 @@ class PostObjectCursor {
 	}
 
 	/**
-	 * Get AND operator for giveno order by key
+	 * Get AND operator for given order by key
 	 *
 	 * @param string    $by The order by key
 	 * @param string    $order The order direction ASC or DESC
@@ -123,12 +140,17 @@ class PostObjectCursor {
 		$post_field = 'post_' . $by;
 		$value = $this->cursor_post->{$post_field};
 
+		/**
+		 * Compare by the post field if the key matches an value
+		 */
 		if ( ! empty( $value ) ) {
 			return $this->compare_with_post_field( $post_field, $value, $order_compare );
 		}
 
+		/**
+		 * Find out whether this is a meta key based ordering
+		 */
 		$meta_key = $this->get_meta_key( $by );
-
 		if ( $meta_key ) {
 			return $this->compare_with_meta_field( $meta_key, $order_compare );
 		}
@@ -141,6 +163,8 @@ class PostObjectCursor {
 	 * Compare using post field
 	 *
 	 * @param string    $by Post field key
+	 * @param string    $value Value from the post object
+	 * @param string    $order_compare comparison string < or >
 	 *
 	 * @return string
 	 */
@@ -163,6 +187,9 @@ class PostObjectCursor {
 		$compare_right = '%s';
 		$compare_left = "{$this->wpdb->postmeta}.meta_value";
 
+		/**
+		 * Cast the compared values if the query has explicit type set
+		 */
 		if ( $meta_type ) {
 			$meta_type = $this->get_cast_for_type( $meta_type );
 			$compare_left = "CAST({$this->wpdb->postmeta}.meta_value AS $meta_type)";
@@ -190,7 +217,10 @@ class PostObjectCursor {
 			return ! empty( $this->query->query_vars["meta_key"] ) ? esc_sql( $this->query->query_vars["meta_key"] ) : null;
 		}
 
-		// Look for meta clause
+		/**
+		 * Check for the WP 4.2+ style meta clauses
+		 * https://make.wordpress.org/core/2015/03/30/query-improvements-in-wp-4-2-orderby-and-meta_query/
+		 */
 		if ( ! isset( $this->query->query_vars['meta_query'][ $by ] ) ) {
 			return null;
 		}
@@ -206,8 +236,6 @@ class PostObjectCursor {
 	 * It's an intance method. No way to call it without creating the instance?
 	 *
 	 * Return the appropriate alias for the given meta type if applicable.
-	 *
-	 * @since 3.7.0
 	 *
 	 * @param string $type MySQL type to cast meta_value.
 	 * @return string MySQL type.
