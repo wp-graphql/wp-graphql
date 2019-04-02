52b219be97187dd0d38f20c5564be45b40482d5e
CodeProKid <ryan.kanner@gmail.com>
2019-04-02 11:03:20 -0600

    Adding docblocks

--- src/Model/CommentAuthor.php
+++ src/Model/CommentAuthor.php
@@ -5,20 +5,50 @@ namespace WPGraphQL\Model;
 
 use GraphQLRelay\Relay;
 
+/**
+ * Class CommentAuthor - Models the CommentAuthor object
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $url
+ *
+ * @package WPGraphQL\Model
+ */
 class CommentAuthor extends Model {
 
+	/**
+	 * Stores the comment author to be modeled
+	 *
+	 * @var array $comment_author
+	 * @access protected
+	 */
 	protected $comment_author;
 
+	/**
+	 * CommentAuthor constructor.
+	 *
+	 * @param array $comment_author The incoming comment author array to be modeled
+	 *
+	 * @throws \Exception
+	 * @access public
+	 */
 	public function __construct( $comment_author ) {
 		$this->comment_author = $comment_author;
 		parent::__construct( 'CommentAuthorObject', $comment_author );
 		$this->init();
 	}
 
+	/**
+	 * Initializes the object
+	 *
+	 * @access protected
+	 * @return void
+	 */
 	protected function init() {
 
 		if ( 'private' === $this->get_visibility() ) {
-			return null;
+			return;
 		}
 
 		if ( empty( $this->fields ) ) {
