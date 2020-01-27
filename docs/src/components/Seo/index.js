013d155606f6baa27c9a54d81d852fab588ad2f3
Jacob Arriola <jacob@zeek.com>
2020-01-26 16:28:39 -0800

    Use correct twitterHandle property for meta element

--- docs/src/components/Seo/index.js
+++ docs/src/components/Seo/index.js
@@ -40,7 +40,7 @@ function Seo({ description, lang, meta, keywords, title }) {
               },
               {
                 name: `twitter:creator`,
-                content: data.site.siteMetadata.author,
+                content: data.site.siteMetadata.twitterHandle,
               },
               {
                 name: `twitter:title`,
@@ -89,7 +89,7 @@ const detailsQuery = graphql`
       siteMetadata {
         title
         description
-        author
+        twitterHandle
       }
     }
   }
