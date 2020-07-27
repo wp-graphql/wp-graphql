83c20de492e521ce2df37b0a2c8289e9410454cc
Jason Bahl <jasonbahl@mac.com>
2020-07-27 11:41:55 -0600

    - code formatting fixes

--- src/Connection/PostObjects.php
+++ src/Connection/PostObjects.php
@@ -123,12 +123,12 @@ class PostObjects {
 		] );
 
 		register_graphql_connection( [
-			'fromType'      => 'HierarchicalContentNode',
-			'fromFieldName' => 'children',
-			'toType'        => 'ContentNode',
+			'fromType'       => 'HierarchicalContentNode',
+			'fromFieldName'  => 'children',
+			'toType'         => 'ContentNode',
 			'connectionArgs' => self::get_connection_args(),
 			'queryClass'     => 'WP_Query',
-			'resolve'       => function( Post $post, $args, $context, $info ) {
+			'resolve'        => function( Post $post, $args, $context, $info ) {
 
 				if ( $post->isRevision ) {
 					$id = $post->parentDatabaseId;
@@ -145,13 +145,13 @@ class PostObjects {
 		] );
 
 		register_graphql_connection( [
-			'fromType'      => 'HierarchicalContentNode',
-			'toType'        => 'ContentNode',
-			'fromFieldName' => 'ancestors',
+			'fromType'       => 'HierarchicalContentNode',
+			'toType'         => 'ContentNode',
+			'fromFieldName'  => 'ancestors',
 			'connectionArgs' => self::get_connection_args(),
 			'queryClass'     => 'WP_Query',
-			'description'   => __( 'Returns ancestors of the node. Default ordered as lowest (closest to the child) to highest (closest to the root).', 'wp-graphql' ),
-			'resolve'       => function( Post $post, $args, $context, $info ) {
+			'description'    => __( 'Returns ancestors of the node. Default ordered as lowest (closest to the child) to highest (closest to the root).', 'wp-graphql' ),
+			'resolve'        => function( Post $post, $args, $context, $info ) {
 				$ancestors = get_ancestors( $post->ID, null, 'post_type' );
 				if ( empty( $ancestors ) || ! is_array( $ancestors ) ) {
 					return null;
