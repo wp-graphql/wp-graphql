9baf5ba42688a2fd7407c38376ac36623615b530
Seagyn Davis <seagyn.davis@leadhome.co.za>
2020-09-16 22:49:36 +0200

    always forget...

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,13 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'cli' => array($vendorDir . '/wp-cli/php-cli-tools/lib'),
-    'WP_CLI' => array($vendorDir . '/wp-cli/wp-cli/php'),
-    'Requests' => array($vendorDir . '/rmccue/requests/library'),
-    'Oxymel' => array($vendorDir . '/nb/oxymel'),
-    'Mustache' => array($vendorDir . '/mustache/mustache/src'),
-    'Hautelook' => array($vendorDir . '/hautelook/phpass/src'),
-    'Flow\\JSONPath\\Test' => array($vendorDir . '/flow/jsonpath/tests'),
-    'Flow\\JSONPath' => array($vendorDir . '/flow/jsonpath/src'),
-    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInit12a14568338eb748682ecb7787c914f7
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit12a14568338eb748682ecb7787c914f7::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire12a14568338eb748682ecb7787c914f7($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire12a14568338eb748682ecb7787c914f7($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
