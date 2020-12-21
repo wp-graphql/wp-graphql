9e5858350f9bfb9f6864cb6b7d6e1e3d2ed85c2f
Jason Bahl <jasonbahl@mac.com>
2020-12-21 13:33:39 -0700

    - update relay dependency

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInit9de0b597b3b512e12655fec9ae8c1974
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit9de0b597b3b512e12655fec9ae8c1974', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit9de0b597b3b512e12655fec9ae8c1974', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit9de0b597b3b512e12655fec9ae8c1974::getInitializer($loader));
         } else {
@@ -52,24 +50,6 @@ class ComposerAutoloaderInit9de0b597b3b512e12655fec9ae8c1974
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit9de0b597b3b512e12655fec9ae8c1974::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire9de0b597b3b512e12655fec9ae8c1974($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire9de0b597b3b512e12655fec9ae8c1974($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
