14cee19384dcfd679e4624238937eb1652c66457
Jason Bahl <jasonbahl@mac.com>
2020-12-30 12:14:06 -0700

    - Update phpstan/constants.php constants to ignore warnings for WP_LANG_DIR - Remove unused $type_registry from ContentTemplate.php

--- phpstan/constants.php
+++ phpstan/constants.php
@@ -2,4 +2,7 @@
 /**
  * Constants defined in this file are to help phpstan analyze code where constants outside the plugin (WordPress core constants, etc) are being used
  */
+
+define( 'WP_LANG_DIR', true );
 define( 'SAVEQUERIES', true );
+
