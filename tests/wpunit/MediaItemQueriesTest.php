471834110455187c3a191cdffb2b7c2034c635f1
rsm0128 <rsm0128@hotmail.com>
2021-03-09 11:58:26 +0900

    updated unit test to mimic a big image upload

--- tests/wpunit/MediaItemQueriesTest.php
+++ tests/wpunit/MediaItemQueriesTest.php
@@ -497,44 +497,15 @@ class MediaItemQueriesTest extends \Codeception\TestCase\WPTestCase {
 	public function testMediaItemNotExistingSizeQuery() {
 
 		/**
+		 * Add a big image size to test
+		 */
+		add_image_size( 'fullscreen', 2560, 0, false );
+
+		/**
 		 * Create an attachment with a post set as it's parent
 		 */
-		$attachment_id = $this->createPostObject( [
-			'post_type'   => 'attachment',
-			'post_content' => 'some description',
-			'post_mime_type' => 'image/jpeg',
-		] );
-
-		$meta_data = [
-			'width' => 300,
-			'height' => 300,
-			'file' => 'example.jpg',
-			'sizes' => [
-				'thumbnail' => [
-					'file' => 'example-thumbnail.jpg',
-					'width' => 150,
-					'height' => 150,
-					'mime-type' => 'image/jpeg',
-				],
-			],
-			'image_meta' => [
-				'aperture' => 0,
-				'credit' => '',
-				'camera' => '',
-				'caption' => '',
-				'created_timestamp' => 0,
-				'copyright' => '',
-				'focal_length' => 0,
-				'iso' => 0,
-				'shutter_speed' => 0,
-				'title' => '',
-				'orientation' => '1',
-				'keywords' => [],
-			],
-		];
-
-		update_post_meta( $attachment_id, '_wp_attachment_metadata', $meta_data );
-		update_post_meta( $attachment_id, '_wp_attached_file', 'example.jpg' );
+		$filename      = ( WPGRAPHQL_PLUGIN_DIR . '/tests/_data/images/test.png' );
+		$attachment_id = $this->factory()->attachment->create_upload_object( $filename );
 
 		/**
 		 * Create the global ID based on the post_type and the created $id
@@ -547,8 +518,8 @@ class MediaItemQueriesTest extends \Codeception\TestCase\WPTestCase {
 		$query = "
 		query {
 			mediaItem(id: \"{$attachment_global_id}\") {
-				srcSet(size: MEDIUM)
-    			sizes(size: MEDIUM)
+				srcSet(size: FULLSCREEN)
+    			sizes(size: FULLSCREEN)
 			}
 		}";
 
@@ -563,11 +534,15 @@ class MediaItemQueriesTest extends \Codeception\TestCase\WPTestCase {
 
 		$this->assertNotEmpty( $mediaItem );
 
-		$this->assertStringContainsString( '/wp-content/uploads/example-thumbnail.jpg 150w', $mediaItem['srcSet'] );
-		$this->assertStringContainsString( '/wp-content/uploads/example.jpg 300w', $mediaItem['srcSet'] );
-
+		$this->assertNotNull( $mediaItem['srcSet'] );
 		$this->assertNotNull( $mediaItem['sizes'] );
-		$this->assertEquals( '(max-width: 300px) 100vw, 300px', $mediaItem['sizes'] );
+
+		$img_atts = wp_get_attachment_image_src( $attachment_id, 'full' );
+		$this->assertNotEmpty( $img_atts );
+
+		$width = $img_atts[1];
+		// compare with (max-width: 1024px) 100vw, 1024px
+		$this->assertEquals( sprintf( '(max-width: %1$dpx) 100vw, %1$dpx', $width ), $mediaItem['sizes'] );
 	}
 
 }
