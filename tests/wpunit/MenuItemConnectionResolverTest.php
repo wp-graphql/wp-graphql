9f9f9b68372ae6b986dfe1e6d4791a0ae3439b30
Mark Kelnar <markkelnar@gmail.com>
2021-04-06 15:37:28 -0500

    Use empty array when no menu locations registered

---
+++ tests/wpunit/MenuItemConnectionResolverTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use WPGraphQL\Data\Connection\MenuItemConnectionResolver;
+
+class MenuItemConnectionResolverTest extends \Codeception\TestCase\WPTestCase {
+
+	public function setUp(): void {
+		// before
+		WPGraphQL::clear_schema();
+		parent::setUp();
+		// your set up methods here
+	}
+
+	public function tearDown(): void {
+		// your tear down methods here
+		WPGraphQL::clear_schema();
+		// then
+		parent::tearDown();
+	}
+
+	public function testMenuItemConnectionResolverWithNoMenuItem() {
+
+		$query = '
+		{
+			menuItems {
+			  nodes {
+				id
+			  }
+			}
+		  }
+		';
+
+		$actual = do_graphql_request( $query );
+		$this->assertEmpty( $actual['data']['menuItems']['nodes'] );
+	}
+}
