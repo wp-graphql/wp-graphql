411a6159807ba539b83978bf071a013c0b90779c
Jason Bahl <jasonbahl@mac.com>
2021-06-10 14:42:02 -0600

    Revert "- remove use of trait"
    This reverts commit 02ecbd1738f5eaa6a194e7f8b255eb682486e852.

--- src/Type/WPObjectType.php
+++ src/Type/WPObjectType.php
@@ -20,6 +20,8 @@ use WPGraphQL\Type\InterfaceType\Node;
  */
 class WPObjectType extends ObjectType {
 
+	use WPInterfaceTrait;
+
 	/**
 	 * Holds the node_interface definition allowing WPObjectTypes
 	 * to easily define themselves as a node type by implementing
@@ -169,48 +171,6 @@ class WPObjectType extends ObjectType {
 		}
 
 		return self::$node_interface;
-
-	}
-
-	/**
-	 * Given an array of interfaces, this gets the Interfaces the Type should implement including
-	 * inherited interfaces.
-	 *
-	 * @param array $interfaces Array of interfaces the type implements
-	 *
-	 * @return array
-	 */
-	protected function get_implemented_interfaces( array $interfaces ): array {
-
-		$new_interfaces = [];
-
-		if ( empty( $interfaces ) ) {
-			return $new_interfaces;
-		}
-
-		foreach ( $interfaces as $interface ) {
-			if ( is_string( $interface ) ) {
-				$interface_type = $this->type_registry->get_type( $interface );
-				if ( $interface_type instanceof InterfaceType ) {
-
-					$interface_interfaces = $interface_type->getInterfaces();
-
-					if ( ! empty( $interface_interfaces ) ) {
-						foreach ( $interface_interfaces as $interface_interface_name => $interface_interface ) {
-							$new_interfaces[ $interface_interface_name ] = $interface_interface;
-						}
-					}
-
-					$new_interfaces[ $interface ] = $interface_type;
-				}
-				continue;
-			}
-			if ( $interface instanceof InterfaceType ) {
-				$new_interfaces[ get_class( $interface ) ] = $interface;
-			}
-		}
-
-		return array_unique( $new_interfaces );
 
 	}
 
