8ac875d8c4f284545c79e7567ca54dc529a4d68d
Jason Bahl <jasonbahl@mac.com>
2021-07-15 16:03:44 -0600

    - deactivate maintenance mode for test environments

--- docker/app.post-setup.sh
+++ docker/app.post-setup.sh
@@ -9,4 +9,7 @@ wp rewrite structure '/%year%/%monthnum%/%postname%/' --allow-root
 wp db export "${DATA_DUMP_DIR}/dump.sql" --allow-root
 
 # If maintenance mode is active, de-activate it
-wp maintenance-mode deactivate
+if ! $( wp maintenance-mode is-active --allow-root ); then
+  echo "Deactivating maintenance mode"
+  wp maintenance-mode deactivate --allow-root
+fi
