43230ccd1ecf56c566ef9249c3e22252a8e1b6e3
Jason Bahl <jasonbahl@mac.com>
2021-07-15 16:34:14 -0600

    - combine tests again, sleep for maintenance mode

--- tests/acceptance/PluginActivatedCest.php
+++ tests/acceptance/PluginActivatedCest.php
@@ -2,8 +2,12 @@
 
 class PluginActivatedCest {
 	public function seePluginActivated( AcceptanceTester $I ) {
+
+		sleep( 5 );
+
 		$I->loginAsAdmin();
 		$I->amOnPluginsPage();
+		$I->activatePlugin( 'wp-graphql' );
 		$I->seePluginActivated( 'wp-graphql' );
 	}
 }
