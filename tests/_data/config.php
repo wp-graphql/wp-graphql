cd40fa5ce317253eff73250766c2a46d654cf856
Jason Bahl <jasonbahl@mac.com>
2021-07-22 07:23:51 -0600

    - prevent core, theme and plugin updates

--- tests/_data/config.php
+++ tests/_data/config.php
@@ -8,3 +8,6 @@ define( 'GRAPHQL_DEBUG', true );
 define( 'WPGRAPHQL_AUTOLOAD', false );
 define( 'WP_AUTO_UPDATE_CORE', false );
 add_filter( 'enable_maintenance_mode', '__return_false' );
+add_filter( 'wp_auto_update_core', '__return_false' );
+add_filter( 'auto_update_plugin', '__return_false' );
+add_filter( 'auto_update_theme', '__return_false' );
