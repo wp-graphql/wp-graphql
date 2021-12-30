bd53583fc1d411df2c5c8467dd8f9cd8b19ebb66
Andrew Matthews <andrew.matthews@wpengine.com>
2021-12-30 13:11:48 -0500

    fix formatting

--- src/Type/ObjectType/RootQuery.php
+++ src/Type/ObjectType/RootQuery.php
@@ -672,7 +672,7 @@ class RootQuery {
 										'check_enabled'  => false,
 									]
 								);
-								$post_id = ! empty( $revisions ) ? array_values( $revisions )[0] : $post_id;
+								$post_id   = ! empty( $revisions ) ? array_values( $revisions )[0] : $post_id;
 							}
 
 							return absint( $post_id ) ? $context->get_loader( 'post' )->load_deferred( $post_id )->then(
