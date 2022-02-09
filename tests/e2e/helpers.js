821661b68327e43f453af82fb40e35a741758417
Jason Bahl <jasonbahl@mac.com>
2022-02-09 15:35:28 -0700

    🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥

--- tests/e2e/helpers.js
+++ tests/e2e/helpers.js
@@ -32,7 +32,6 @@ export const loadGraphiQL = async ( queryParams = { query: null, variables: null
 
     await Promise.all([
         visitAdminPage('admin.php', `?page=graphiql-ide${_queryParams}` ),
-        page.waitForNavigation( { waitUntil: 'networkidle0' } ),
         page.waitForSelector('#graphiql .graphiql-container', { visible: true, timeout: 5000 }).catch( error => {
             console.log( `failed to wait for graphiql...`)
         })
