8b35639e6f3eb2c0fbc00d709860f1110380b605
Jason Bahl <jasonbahl@mac.com>
2022-02-10 15:56:27 -0700

    - change dashes for underscores. Might cry if this works. The tester page should fail, graphql pages should work.

--- tests/e2e/graphiql.test.js
+++ tests/e2e/graphiql.test.js
@@ -16,12 +16,12 @@ describe('Graphiql', function () {
     })
 
     it ('can open graphiql admin page', async () => {
-        await visitAdminPage( 'admin.php', 'page=graphiql_ide' );
+        await visitAdminPage( 'admin.php', 'page=graphiql-ide' );
         expect( true ).toBeTruthy();
     })
 
     it ('can open graphql settings page', async () => {
-        await visitAdminPage( 'admin.php', 'page=graphql_settings' );
+        await visitAdminPage( 'admin.php', 'page=graphql-settings' );
         expect( true ).toBeTruthy();
     })
 
