d757e4c282f45ac367414835f71ac03b26aeccaa
Jason Bahl <jasonbahl@mac.com>
2022-02-11 14:49:20 -0700

    - tryin again 👀

--- tests/e2e/helpers.js
+++ tests/e2e/helpers.js
@@ -30,9 +30,9 @@ export const loadGraphiQL = async ( queryParams = { query: null, variables: null
 
     _queryParams += `&explorerIsOpen=${explorerIsOpen ? "1" : "false" }`
 
-    await Promise.all([
-        visitAdminPage('admin.php', `?page=graphiql-ide${_queryParams}` ),
-    ]);
+    await visitAdminPage('admin.php', `?page=graphiql-ide${_queryParams}` );
+    await wait( 5000 );
+    await page.waitForSelector('#graphiql .graphiql-container', { visible: true, timeout: 30000 });
 
 
 }
