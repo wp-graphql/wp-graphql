da114b224081659c5479cdff7815a4ba28d9696b
Dovid Levine <justlevine@gmail.com>
2022-03-11 17:39:46 +0000

    chore: move template object registration out of TypeRegistry

--- src/Registry/TypeRegistry.php
+++ src/Registry/TypeRegistry.php
@@ -280,6 +280,7 @@ class TypeRegistry {
 		Avatar::register_type();
 		Comment::register_type();
 		CommentAuthor::register_type();
+		ContentTemplate::register_content_template_types();
 		EnqueuedStylesheet::register_type();
 		EnqueuedScript::register_type();
 		MediaDetails::register_type();
@@ -362,56 +363,6 @@ class TypeRegistry {
 		UserUpdate::register_mutation();
 		UserRegister::register_mutation();
 		UpdateSettings::register_mutation( $this );
-
-		$registered_page_templates = wp_get_theme()->get_post_templates();
-
-		$page_templates['default'] = 'DefaultTemplate';
-
-		if ( ! empty( $registered_page_templates ) && is_array( $registered_page_templates ) ) {
-
-			foreach ( $registered_page_templates as $post_type_templates ) {
-				if ( ! empty( $post_type_templates ) && is_array( $post_type_templates ) ) {
-					foreach ( $post_type_templates as $file => $name ) {
-						$page_templates[ $file ] = $name;
-					}
-				}
-			}
-		}
-
-		if ( ! empty( $page_templates ) && is_array( $page_templates ) ) {
-
-			foreach ( $page_templates as $file => $name ) {
-				$name          = ucwords( $name );
-				$replaced_name = preg_replace( '/[^\w]/', '', $name );
-
-				if ( ! empty( $replaced_name ) ) {
-					$name = $replaced_name;
-				}
-
-				if ( preg_match( '/^\d/', $name ) || false === strpos( strtolower( $name ), 'template' ) ) {
-					$name = 'Template_' . $name;
-				}
-				$template_type_name = $name;
-
-				register_graphql_object_type(
-					$template_type_name,
-					[
-						'interfaces'      => [ 'ContentTemplate' ],
-						// Translators: Placeholder is the name of the GraphQL Type in the Schema
-						'description'     => __( 'The template assigned to the node', 'wp-graphql' ),
-						'fields'          => [
-							'templateName' => [
-								'resolve' => function ( $template ) {
-									return isset( $template['templateName'] ) ? $template['templateName'] : null;
-								},
-							],
-						],
-						'eagerlyLoadType' => true,
-					]
-				);
-
-			}
-		}
 
 		/**
 		 * Register PostObject types based on post_types configured to show_in_graphql
--- src/Type/InterfaceType/ContentTemplate.php
+++ src/Type/InterfaceType/ContentTemplate.php
@@ -26,4 +26,61 @@ class ContentTemplate {
 			]
 		);
 	}
+
+	/**
+	 * Register individual GraphQL objects for supported theme templates.
+	 *
+	 * @return void
+	 */
+	public static function register_content_template_types() {
+		$registered_page_templates = wp_get_theme()->get_post_templates();
+
+		$page_templates['default'] = 'DefaultTemplate';
+
+		if ( ! empty( $registered_page_templates ) && is_array( $registered_page_templates ) ) {
+
+			foreach ( $registered_page_templates as $post_type_templates ) {
+				if ( ! empty( $post_type_templates ) && is_array( $post_type_templates ) ) {
+					foreach ( $post_type_templates as $file => $name ) {
+						$page_templates[ $file ] = $name;
+					}
+				}
+			}
+		}
+
+		if ( ! empty( $page_templates ) && is_array( $page_templates ) ) {
+
+			foreach ( $page_templates as $file => $name ) {
+				$name          = ucwords( $name );
+				$replaced_name = preg_replace( '/[^\w]/', '', $name );
+
+				if ( ! empty( $replaced_name ) ) {
+					$name = $replaced_name;
+				}
+
+				if ( preg_match( '/^\d/', $name ) || false === strpos( strtolower( $name ), 'template' ) ) {
+					$name = 'Template_' . $name;
+				}
+				$template_type_name = $name;
+
+				register_graphql_object_type(
+					$template_type_name,
+					[
+						'interfaces'      => [ 'ContentTemplate' ],
+						// Translators: Placeholder is the name of the GraphQL Type in the Schema
+						'description'     => __( 'The template assigned to the node', 'wp-graphql' ),
+						'fields'          => [
+							'templateName' => [
+								'resolve' => function ( $template ) {
+									return isset( $template['templateName'] ) ? $template['templateName'] : null;
+								},
+							],
+						],
+						'eagerlyLoadType' => true,
+					]
+				);
+
+			}
+		}
+	}
 }
