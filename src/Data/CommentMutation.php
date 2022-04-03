bcbe67450a65513168ca1af1744709f7d7e3b65d
Dovid Levine <justlevine@gmail.com>
2022-04-03 01:51:02 +0000

    chore: refactor logic to get_comment_author()

--- src/Data/CommentMutation.php
+++ src/Data/CommentMutation.php
@@ -42,17 +42,15 @@ class CommentMutation {
 		 *    'comment_approved' => 1,
 		 */
 
-		$user = wp_get_current_user();
-		if ( 0 !== $user->ID ) {
-			// This is only ever the ID of the current user.
+		$user = self::get_comment_author( $input['authorEmail'] ?? null );
+
+		if ( false !== $user ) {
+
 			$output_args['user_id'] = $user->ID;
 
-			// Only users with the right permissions should be able to set a different author than themselves.
-			$can_moderate_comments = current_user_can( 'moderate_comments' );
-
-			$input['author']      = $can_moderate_comments && ! empty( $input['author'] ) ? $input['author'] : $user->display_name;
-			$input['authorEmail'] = $can_moderate_comments && ! empty( $input['authorEmail'] ) ? $input['authorEmail'] : $user->user_email;
-			$input['authorUrl']   = $can_moderate_comments && ! empty( $input['authorUrl'] ) ? $input['authorUrl'] : $user->user_url;
+			$input['author']      = ! empty( $input['author'] ) ? $input['author'] : $user->display_name;
+			$input['authorEmail'] = ! empty( $input['authorEmail'] ) ? $input['authorEmail'] : $user->user_email;
+			$input['authorUrl']   = ! empty( $input['authorUrl'] ) ? $input['authorUrl'] : $user->user_url;
 		}
 
 		if ( empty( $input['author'] ) ) {
@@ -130,5 +128,30 @@ class CommentMutation {
 		$default_comment_status  = 0;
 
 		do_action( 'graphql_comment_object_mutation_update_additional_data', $comment_id, $input, $mutation_name, $context, $info, $intended_comment_status, $default_comment_status );
+	}
+
+	/**
+	 * Gets the user object for the comment author.
+	 *
+	 * @param ?string $author_email The authorEmail provided to the mutation input.
+	 *
+	 * @return \WP_User|false
+	 */
+	protected static function get_comment_author( string $author_email = null ) {
+		$user = wp_get_current_user();
+
+		// Fail if no logged in user.
+		if ( 0 === $user->ID ) {
+			return false;
+		}
+
+		// Return the current user if they can only handle their own comments or if there's no specified author.
+		if ( empty( $author_email ) || ! $user->has_cap( 'moderate_comments' ) ) {
+			return $user;
+		}
+
+		$author = get_user_by( 'email', $author_email );
+
+		return ! empty( $author->ID ) ? $author : false;
 	}
 }
