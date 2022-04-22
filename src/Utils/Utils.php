c0b3ad2fd3c3c613a5aa2d36c784968e97d3bee1
Jason Bahl <jasonbahl@mac.com>
2022-04-22 12:31:15 -0600

    - fix code formatting

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -155,7 +155,7 @@ class Utils {
 			'iframe'   => $allowed_atts,
 			'script'   => $allowed_atts,
 			'select'   => $allowed_atts,
-            		'option'   => $allowed_atts,
+			'option'   => $allowed_atts,
 			'style'    => $allowed_atts,
 			'strong'   => $allowed_atts,
 			'small'    => $allowed_atts,
