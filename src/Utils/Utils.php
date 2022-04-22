c82183aebb2f0db2888fdd15e4d68faf2698d846
Jayme Edwards <jayme.edwards@gmail.com>
2022-04-22 18:41:46 +0000

    Selected attribute added to allow select settings to work.

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -145,6 +145,7 @@ class Utils {
 			'title'      => [],
 			'checked'    => [],
 			'disabled'   => [],
+            'selected'   => [],
 		];
 
 		return [
@@ -155,7 +156,7 @@ class Utils {
 			'iframe'   => $allowed_atts,
 			'script'   => $allowed_atts,
 			'select'   => $allowed_atts,
-            		'option'   => $allowed_atts,
+            'option'   => $allowed_atts,
 			'style'    => $allowed_atts,
 			'strong'   => $allowed_atts,
 			'small'    => $allowed_atts,
