0a01432a2283bec41f6a3a04f9a17de9b6f6aa26
Jason Bahl <jasonbahl@mac.com>
2022-05-13 16:36:24 -0600

    - remove UniformResourceIdentifiable from `MenuItem`

--- src/Type/ObjectType/MenuItem.php
+++ src/Type/ObjectType/MenuItem.php
@@ -21,7 +21,7 @@ class MenuItem {
 			'MenuItem',
 			[
 				'description' => __( 'Navigation menu items are the individual items assigned to a menu. These are rendered as the links in a navigation menu.', 'wp-graphql' ),
-				'interfaces'  => [ 'Node', 'DatabaseIdentifier', 'UniformResourceIdentifiable' ],
+				'interfaces'  => [ 'Node', 'DatabaseIdentifier' ],
 				'connections' => [
 					'connectedNode' => [
 						'toType'               => 'MenuItemLinkable',
