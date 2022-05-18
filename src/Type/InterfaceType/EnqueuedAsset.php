e01522caffa0c7cec01939dd2d5a289b88efeaaa
Dovid Levine <justlevine@gmail.com>
2022-05-18 19:36:21 +0000

    fix: resolve asset versions

--- src/Type/InterfaceType/EnqueuedAsset.php
+++ src/Type/InterfaceType/EnqueuedAsset.php
@@ -52,7 +52,7 @@ class EnqueuedAsset {
 				],
 				'version'      => [
 					'type'        => 'String',
-					'description' => __( 'The version of the enqueued asset', 'wp-graphql' ),
+					'description' => __( 'The version of the enqueued asset. Defaults to the current version of WordPress.', 'wp-graphql' ),
 				],
 				'src'          => [
 					'type'        => 'String',
