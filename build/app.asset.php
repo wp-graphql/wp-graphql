57f1e80cae1016bb79e81afb26582537f2018fd7
Jason Bahl <jasonbahl@mac.com>
2022-07-15 15:44:57 -0600

    - update Router.js to use items instead of children for the Menu component - update tests to work with updated testing library changes

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks'), 'version' => '55b8a418b5af44ebd144');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks'), 'version' => '097e8de98d3d027593db');
