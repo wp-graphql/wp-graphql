bdf3ed5aca0a6ec2da4f52cf7db8864b824af6b3
David Levine <david@axepress.dev>
2022-09-18 06:54:28 +0000

    chore: sort connection args

--- src/Connection/TermObjects.php
+++ src/Connection/TermObjects.php
@@ -243,6 +243,58 @@ class TermObjects {
 	public static function get_connection_args( $args = [] ) {
 		return array_merge(
 			[
+				'childless'           => [
+					'type'        => 'Boolean',
+					'description' => __( 'True to limit results to terms that have no children. This parameter has no effect on non-hierarchical taxonomies. Default false.', 'wp-graphql' ),
+				],
+				'childOf'             => [
+					'type'        => 'Int',
+					'description' => __( 'Term ID to retrieve child terms of. If multiple taxonomies are passed, $child_of is ignored. Default 0.', 'wp-graphql' ),
+				],
+				'cacheDomain'         => [
+					'type'        => 'String',
+					'description' => __( 'Unique cache key to be produced when this query is stored in an object cache. Default is \'core\'.', 'wp-graphql' ),
+				],
+				'descriptionLike'     => [
+					'type'        => 'String',
+					'description' => __( 'Retrieve terms where the description is LIKE the input value. Default empty.', 'wp-graphql' ),
+				],
+				'exclude'             => [ // phpcs:ignore WordPressVIPMinimum.Performance.WPQueryParams.PostNotIn_exclude
+					'type'        => [
+						'list_of' => 'ID',
+					],
+					'description' => __( 'Array of term ids to exclude. If $include is non-empty, $exclude is ignored. Default empty array.', 'wp-graphql' ),
+				],
+				'excludeTree'         => [
+					'type'        => [
+						'list_of' => 'ID',
+					],
+					'description' => __( 'Array of term ids to exclude along with all of their descendant terms. If $include is non-empty, $exclude_tree is ignored. Default empty array.', 'wp-graphql' ),
+				],
+				'hideEmpty'           => [
+					'type'        => 'Boolean',
+					'description' => __( 'Whether to hide terms not assigned to any posts. Accepts true or false. Default false', 'wp-graphql' ),
+				],
+				'hierarchical'        => [
+					'type'        => 'Boolean',
+					'description' => __( 'Whether to include terms that have non-empty descendants (even if $hide_empty is set to true). Default true.', 'wp-graphql' ),
+				],
+				'include'             => [
+					'type'        => [
+						'list_of' => 'ID',
+					],
+					'description' => __( 'Array of term ids to include. Default empty array.', 'wp-graphql' ),
+				],
+				'name'                => [
+					'type'        => [
+						'list_of' => 'String',
+					],
+					'description' => __( 'Array of names to return term(s) for. Default empty.', 'wp-graphql' ),
+				],
+				'nameLike'            => [
+					'type'        => 'String',
+					'description' => __( 'Retrieve terms where the name is LIKE the input value. Default empty.', 'wp-graphql' ),
+				],
 				'objectIds'           => [
 					'type'        => [
 						'list_of' => 'ID',
@@ -257,33 +309,17 @@ class TermObjects {
 					'type'        => 'OrderEnum',
 					'description' => __( 'Direction the connection should be ordered in', 'wp-graphql' ),
 				],
-				'hideEmpty'           => [
+				'padCounts'           => [
 					'type'        => 'Boolean',
-					'description' => __( 'Whether to hide terms not assigned to any posts. Accepts true or false. Default false', 'wp-graphql' ),
+					'description' => __( 'Whether to pad the quantity of a term\'s children in the quantity of each term\'s "count" object variable. Default false.', 'wp-graphql' ),
 				],
-				'include'             => [
-					'type'        => [
-						'list_of' => 'ID',
-					],
-					'description' => __( 'Array of term ids to include. Default empty array.', 'wp-graphql' ),
+				'parent'              => [
+					'type'        => 'Int',
+					'description' => __( 'Parent term ID to retrieve direct-child terms of. Default empty.', 'wp-graphql' ),
 				],
-				'exclude'             => [ // phpcs:ignore WordPressVIPMinimum.Performance.WPQueryParams.PostNotIn_exclude
-					'type'        => [
-						'list_of' => 'ID',
-					],
-					'description' => __( 'Array of term ids to exclude. If $include is non-empty, $exclude is ignored. Default empty array.', 'wp-graphql' ),
-				],
-				'excludeTree'         => [
-					'type'        => [
-						'list_of' => 'ID',
-					],
-					'description' => __( 'Array of term ids to exclude along with all of their descendant terms. If $include is non-empty, $exclude_tree is ignored. Default empty array.', 'wp-graphql' ),
-				],
-				'name'                => [
-					'type'        => [
-						'list_of' => 'String',
-					],
-					'description' => __( 'Array of names to return term(s) for. Default empty.', 'wp-graphql' ),
+				'search'              => [
+					'type'        => 'String',
+					'description' => __( 'Search criteria to match terms. Will be SQL-formatted with wildcards before and after. Default empty.', 'wp-graphql' ),
 				],
 				'slug'                => [
 					'type'        => [
@@ -296,42 +332,6 @@ class TermObjects {
 						'list_of' => 'ID',
 					],
 					'description' => __( 'Array of term taxonomy IDs, to match when querying terms.', 'wp-graphql' ),
-				],
-				'hierarchical'        => [
-					'type'        => 'Boolean',
-					'description' => __( 'Whether to include terms that have non-empty descendants (even if $hide_empty is set to true). Default true.', 'wp-graphql' ),
-				],
-				'search'              => [
-					'type'        => 'String',
-					'description' => __( 'Search criteria to match terms. Will be SQL-formatted with wildcards before and after. Default empty.', 'wp-graphql' ),
-				],
-				'nameLike'            => [
-					'type'        => 'String',
-					'description' => __( 'Retrieve terms where the name is LIKE the input value. Default empty.', 'wp-graphql' ),
-				],
-				'descriptionLike'     => [
-					'type'        => 'String',
-					'description' => __( 'Retrieve terms where the description is LIKE the input value. Default empty.', 'wp-graphql' ),
-				],
-				'padCounts'           => [
-					'type'        => 'Boolean',
-					'description' => __( 'Whether to pad the quantity of a term\'s children in the quantity of each term\'s "count" object variable. Default false.', 'wp-graphql' ),
-				],
-				'childOf'             => [
-					'type'        => 'Int',
-					'description' => __( 'Term ID to retrieve child terms of. If multiple taxonomies are passed, $child_of is ignored. Default 0.', 'wp-graphql' ),
-				],
-				'parent'              => [
-					'type'        => 'Int',
-					'description' => __( 'Parent term ID to retrieve direct-child terms of. Default empty.', 'wp-graphql' ),
-				],
-				'childless'           => [
-					'type'        => 'Boolean',
-					'description' => __( 'True to limit results to terms that have no children. This parameter has no effect on non-hierarchical taxonomies. Default false.', 'wp-graphql' ),
-				],
-				'cacheDomain'         => [
-					'type'        => 'String',
-					'description' => __( 'Unique cache key to be produced when this query is stored in an object cache. Default is \'core\'.', 'wp-graphql' ),
 				],
 				'updateTermMetaCache' => [
 					'type'        => 'Boolean',
