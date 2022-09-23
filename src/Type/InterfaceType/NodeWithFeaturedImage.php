6d9529b6899eac537cbe2e4c8abf443c3995bfc8
Jason Bahl <jasonbahl@mac.com>
2022-09-22 19:49:20 -0600

    - remove `DatabaseIdentifier` from `NodeWithFeaturedImage` to be more consistent with the other `NodeBy*` interfaces

--- src/Type/InterfaceType/NodeWithFeaturedImage.php
+++ src/Type/InterfaceType/NodeWithFeaturedImage.php
@@ -24,7 +24,7 @@ class NodeWithFeaturedImage {
 			'NodeWithFeaturedImage',
 			[
 				'description' => __( 'A node that can have a featured image set', 'wp-graphql' ),
-				'interfaces'  => [ 'Node', 'DatabaseIdentifier' ],
+				'interfaces'  => [ 'Node' ],
 				'connections' => [
 					'featuredImage' => [
 						'toType'   => 'MediaItem',
