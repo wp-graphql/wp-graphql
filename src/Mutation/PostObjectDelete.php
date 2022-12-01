9655c9cfc053e0c2ce6caa3e18b9ae3ebe3952be
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2022-12-01 11:06:59 -0600

    Update src/Mutation/PostObjectDelete.php
    Co-authored-by: Jason Bahl <jasonbahl@mac.com>

--- src/Mutation/PostObjectDelete.php
+++ src/Mutation/PostObjectDelete.php
@@ -55,7 +55,7 @@ class PostObjectDelete {
 			],
 			'ignoreEditLock' => [
 				'type' => 'Boolean',
-				'description' => __( 'Override the edit lock when another user is editting the post', 'wp-graphq' ),
+				'description' => __( 'Override the edit lock when another user is editing the post', 'wp-graphql' ),
 			],
 		];
 	}
