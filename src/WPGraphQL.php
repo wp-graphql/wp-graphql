d85f0ce884f64b282ab5b829b18735f596354f22
Jason Bahl <jasonbahl@mac.com>
2023-03-02 10:12:22 -0700

    - update versions, changelog for release

--- src/WPGraphQL.php
+++ src/WPGraphQL.php
@@ -130,7 +130,7 @@ final class WPGraphQL {
 
 		// Plugin version.
 		if ( ! defined( 'WPGRAPHQL_VERSION' ) ) {
-			define( 'WPGRAPHQL_VERSION', '1.13.9' );
+			define( 'WPGRAPHQL_VERSION', '1.13.10' );
 		}
 
 		// Plugin Folder Path.
