94b48098956a89bbc7b6cb59757f2250160fe87a
Blake Wilson <blake@blake.id>
2023-03-15 15:42:22 -0500

    Create test

---
+++ tests/wpunit/IsToolbarVisibleQueryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+class IsToolbarVisibleQueryTest extends \Codeception\TestCase\WPTestCase {
+
+	public function setUp(): void {
+		parent::setUp();
+		WPGraphQL::clear_schema();
+	}
+
+	public function tearDown(): void {
+		WPGraphQL::clear_schema();
+		parent::tearDown();
+	}
+
+	public function testViewerQuery() {
+
+		$user_id = $this->factory->user->create( [
+			'role' => 'administrator',
+		] );
+
+		$query = '
+		{
+		  viewer{
+		    userId
+		    roles {
+		        nodes {
+		          name
+		        }
+		    }
+        isToolbarVisible
+		  }
+		}
+		';
+
+		/**
+		 * Set the current user so we can properly test the viewer query
+		 */
+		wp_set_current_user( $user_id );
+
+		$actual = graphql( [ 'query' => $query ] );
+
+		codecept_debug( $actual );
+
+		$this->assertArrayNotHasKey( 'errors', $actual );
+		$this->assertEquals( $user_id, $actual['data']['viewer']['userId'] );
+    $this->assertSame(true, $actual['data']['viewer']['isToolbarVisible']);
+
+    // Update the user's preference to not show admin bar.
+    update_user_meta($user_id, 'show_admin_bar_front', "false");
+
+		$actual = graphql( [ 'query' => $query ] );
+
+		codecept_debug( $actual );
+
+		$this->assertArrayNotHasKey( 'errors', $actual );
+		$this->assertEquals( $user_id, $actual['data']['viewer']['userId'] );
+    $this->assertSame(false, $actual['data']['viewer']['isToolbarVisible']);
+	}
+
+}
