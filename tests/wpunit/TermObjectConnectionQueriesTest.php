d7dba7fac8d52952ce4e6a7104d4482b6c65b4b9
Jason Bahl <jasonbahl@mac.com>
2023-04-12 10:47:59 -0600

    - fix cleanup on test

--- tests/wpunit/TermObjectConnectionQueriesTest.php
+++ tests/wpunit/TermObjectConnectionQueriesTest.php
@@ -748,9 +748,9 @@ class TermObjectConnectionQueriesTest extends \Tests\WPGraphQL\TestCase\WPGraphQ
 
 		$this->assertSame( $actual_ancestor_ids, $expected_ancestor_ids );
 
-		wp_delete_term( $parent );
-		wp_delete_term( $child );
-		wp_delete_term( $grandchild );
+		wp_delete_term( $parent, 'category' );
+		wp_delete_term( $child, 'category' );
+		wp_delete_term( $grandchild, 'category' );
 
 	}
 
