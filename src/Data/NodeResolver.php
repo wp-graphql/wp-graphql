fb91522b88ebfe326e78f1d32170a01eeea879e9
Blake Wilson <blake@blake.id>
2023-09-25 14:04:27 -0500

    Removing the union type as this was introduced in PHP 8.2 and the current required PHP version is 7.1

--- src/Data/NodeResolver.php
+++ src/Data/NodeResolver.php
@@ -595,7 +595,7 @@ class NodeResolver {
 	 * 
 	 * @return Int A preview post ID if one exists, the current post ID if one doesn't exist.
 	 */
-	public static function resolve_node_preview( int | WP_Post $post ) {
+	public static function resolve_node_preview( $post ) {
 		$post_id = is_object($post) ? $post->ID : $post;
 
 		$revisions = wp_get_post_revisions(
