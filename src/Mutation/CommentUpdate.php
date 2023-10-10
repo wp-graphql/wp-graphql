5e3c9be9db91a31051e37baa1abb9aad22784430
Jason Bahl <jasonbahl@mac.com>
2023-10-10 14:48:50 -0600

    - update CommentUpdate mutation to return error message if comment cannot be updated - return success:false if comment isn't updated, but an error did not occur (i.e. there were no changes to the comment from the mutation input, etc)

--- src/Mutation/CommentUpdate.php
+++ src/Mutation/CommentUpdate.php
@@ -79,7 +79,8 @@ class CommentUpdate {
 			/**
 			 * Map all of the args from GraphQL to WordPress friendly args array
 			 */
-			$user_id = isset( $comment_args['user_id'] ) ? $comment_args['user_id'] : null;
+			$user_id = $comment_args['user_id'] ?? null;
+			$raw_comment_args = $comment_args;
 			CommentMutation::prepare_comment_object( $input, $comment_args, 'update', true );
 
 			// Prevent comment deletions by default
@@ -105,17 +106,22 @@ class CommentUpdate {
 				throw new UserError( __( 'Sorry, you are not allowed to update this comment.', 'wp-graphql' ) );
 			}
 
+			// If there are no changes between the existing comment and the incoming comment
+			if ( $comment_args === $raw_comment_args ) {
+				throw new UserError( __( 'No changes have been provided to the comment.', 'wp-graphql' ) );
+			}
+
 			/**
 			 * Update comment
 			 * $success   int   1 on success and 0 on fail
 			 */
-			$success = wp_update_comment( $comment_args );
+			$success = wp_update_comment( $comment_args, true );
 
 			/**
 			 * Throw an exception if the comment failed to be created
 			 */
-			if ( ! $success ) {
-				throw new UserError( __( 'The comment failed to update', 'wp-graphql' ) );
+			if ( is_wp_error( $success ) ) {
+				throw new UserError( $success->get_error_message() );
 			}
 
 			/**
@@ -131,6 +137,7 @@ class CommentUpdate {
 			 */
 			return [
 				'id' => $comment_id,
+				'success' => (bool) $success,
 			];
 		};
 	}
