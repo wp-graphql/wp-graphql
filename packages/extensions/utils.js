868b7c1963c9ad3a0da8a0109292341f24cf1a2d
Joe Fusco <joe.fusco@wpengine.com>
2024-07-31 12:50:46 -0400

    improve sanitization in utils

--- packages/extensions/utils.js
+++ packages/extensions/utils.js
@@ -1,10 +1,28 @@
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Returns the details for the button based on the plugin status and host.
+ *
+ * @param {string} host - The host name where the plugin is being used.
+ * @param {string} plugin_url - The URL of the plugin.
+ * @param {boolean} isInstalled - Whether the plugin is installed.
+ * @param {boolean} isActive - Whether the plugin is active.
+ * @param {boolean} installing - Whether the plugin is currently being installed.
+ * @param {boolean} activating - Whether the plugin is currently being activated.
+ * @param {Function} activatePlugin - Function to activate the plugin.
+ * @returns {{buttonText: string, buttonDisabled: boolean, buttonOnClick: Function|null}} The button details.
+ */
 export const getButtonDetails = (host, plugin_url, isInstalled, isActive, installing, activating, activatePlugin) => {
     let buttonText;
     let buttonDisabled = false;
-    let buttonOnClick;
+    let buttonOnClick = null;
 
+    /**
+     * Opens a new browser window with the specified URL.
+     *
+     * @param {string} url - The URL to open.
+     * @returns {Function} A function that opens the URL in a new window.
+     */
     const openLink = (url) => () => window.open(url, '_blank');
 
     if (installing) {
@@ -20,21 +38,22 @@ export const getButtonDetails = (host, plugin_url, isInstalled, isActive, instal
         buttonText = __('Activate', 'wp-graphql');
         buttonOnClick = activatePlugin;
     } else {
-        // Using `true` for readability to handle multiple `case` conditions
+        const domain = new URL(plugin_url).hostname.toLowerCase();
+
         switch (true) {
-            case host.includes('github.com'):
+            case /github\.com$/.test(domain):
                 buttonText = __('View on GitHub', 'wp-graphql');
                 buttonOnClick = openLink(plugin_url);
                 break;
-            case host.includes('bitbucket.org'):
+            case /bitbucket\.org$/.test(domain):
                 buttonText = __('View on Bitbucket', 'wp-graphql');
                 buttonOnClick = openLink(plugin_url);
                 break;
-            case host.includes('gitlab.com'):
+            case /gitlab\.com$/.test(domain):
                 buttonText = __('View on GitLab', 'wp-graphql');
                 buttonOnClick = openLink(plugin_url);
                 break;
-            case host.includes('wordpress.org'):
+            case /wordpress\.org$/.test(domain):
                 buttonText = __('Install & Activate', 'wp-graphql');
                 buttonOnClick = activatePlugin;
                 break;
