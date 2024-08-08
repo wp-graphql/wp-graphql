88fd31cef66ac0981d0cca300173da57f7268bfa
Jason Bahl <jasonbahl@mac.com>
2024-08-07 21:16:28 -0600

    - adjust query based on feedback. Select from the USER table instead of post_author table as most sites have fewer users than posts so this requires less compute to complete

--- src/Data/Loader/UserLoader.php
+++ src/Data/Loader/UserLoader.php
@@ -78,7 +78,7 @@ class UserLoader extends AbstractDataLoader {
 
 		$results = $wpdb->get_results( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching
 			$wpdb->prepare(
-				"SELECT DISTINCT post_author FROM $wpdb->posts INNER JOIN $wpdb->users ON post_author = $wpdb->users.ID $where AND post_author IN ( $ids ) ORDER BY FIELD( post_author, $ids)" // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPressVIPMinimum.Variables.RestrictedVariables.user_meta__wpdb__users
+				"SELECT DISTINCT $wpdb->users.ID FROM $wpdb->posts INNER JOIN $wpdb->users ON post_author = $wpdb->users.ID $where AND post_author IN ( $ids ) ORDER BY FIELD( $wpdb->users.ID, $ids)" // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPressVIPMinimum.Variables.RestrictedVariables.user_meta__wpdb__users
 			)
 		);
 
@@ -95,7 +95,7 @@ class UserLoader extends AbstractDataLoader {
 		return array_reduce(
 			$results,
 			static function ( $carry, $result ) {
-				$carry[ (int) $result->post_author ] = true;
+				$carry[ (int) $result->ID ] = true;
 				return $carry;
 			},
 			[]
