9a5efb96198247c656f5ad722a665911db7da19f
Geoff Taylor <geoff@axistaylor.com>
2024-08-16 17:00:49 -0400

    fix: unnecessary code change removed

--- src/Type/ObjectType/EnqueuedScript.php
+++ src/Type/ObjectType/EnqueuedScript.php
@@ -64,9 +64,7 @@ class EnqueuedScript {
 						'type'        => 'ScriptLoadingGroupEnum',
 						'description' => __( 'The location where this script should be loaded', 'wp-graphql' ),
 						'resolve'     => static function ( \_WP_Dependency $script ) {
-							if ( isset( $script->args ) && 1 === $script->args ) {
-                                return 1;
-                            } elseif ( ! isset( $script->extra['group'] ) ) {
+							if ( ! isset( $script->extra['group'] ) ) {
                                 return 0;
                             }
                             // graphql_debug([$script->handle => $script]);
