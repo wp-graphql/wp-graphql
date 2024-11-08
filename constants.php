1ce82fa3aa731a411a3370628908cb090b1a67de
Jason Bahl <jasonbahl@mac.com>
2024-11-07 19:57:27 -0700

    - update changelog, version for release

--- constants.php
+++ constants.php
@@ -18,7 +18,7 @@ function graphql_setup_constants() {
 
 	// Plugin version.
 	if ( ! defined( 'WPGRAPHQL_VERSION' ) ) {
-		define( 'WPGRAPHQL_VERSION', '1.29.0' );
+		define( 'WPGRAPHQL_VERSION', '1.29.1' );
 	}
 
 	// Plugin Folder Path.
