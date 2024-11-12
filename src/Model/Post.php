19488bab9a8f66bcc4f39287a661ca3a5c767c49
izzygld <izzygld@gmail.com>
2024-11-12 14:40:19 +0200

    update: empty title to be a empty string

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -482,7 +482,7 @@ class Post extends Model {
 				],
 				'titleRendered'             => function () {
 					$id    = ! empty( $this->data->ID ) ? $this->data->ID : null;
-					$title = ! empty( $this->data->post_title ) ? $this->data->post_title : null;
+					$title = ! empty( $this->data->post_title ) ? $this->data->post_title : '';
 
 					return $this->html_entity_decode( apply_filters( 'the_title', $title, $id ), 'titleRendered', true );
 				},
