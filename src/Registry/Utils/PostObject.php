46b9a6e6d65d30192eb0bf6275c6b310725f5010
SrPatinhas <migas_cerejo-1993@hotmail.com>
2024-11-29 18:56:49 +0000

    feat: add option to get filesize externally

--- src/Registry/Utils/PostObject.php
+++ src/Registry/Utils/PostObject.php
@@ -613,6 +613,11 @@ class PostObject {
 						if ( empty( $source_url ) ) {
 							return null;
 						}
+						// allow to get filesize from other sources, like database or offloaded file
+						$filesize = apply_filter('graphql_get_filesize', $image->databaseId);
+						if(!empty($filesize)) {
+							return $filesize;
+						}
 
 						$path_parts    = pathinfo( $source_url );
 						$original_file = get_attached_file( absint( $image->databaseId ) );
