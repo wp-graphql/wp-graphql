a8cb27ef84c73a75a4e368e871dec3b549d48c26
Jason Bahl <jasonbahl@mac.com>
2025-01-15 17:28:16 -0700

    - update logic to also check if $append is true

--- src/Data/PostObjectMutation.php
+++ src/Data/PostObjectMutation.php
@@ -355,7 +355,7 @@ class PostObjectMutation {
 						return;
 					}
 
-					if ( 'category' === $tax_object->name ) {
+					if ( $append && 'category' === $tax_object->name ) {
 						$default_category_id = absint( get_option( 'default_category' ) );
 						if ( ! in_array( $default_category_id, $terms_to_connect, true ) ) {
 							wp_remove_object_terms( $post_id, $default_category_id, 'category' );
