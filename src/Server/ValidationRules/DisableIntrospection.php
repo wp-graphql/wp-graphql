c4765911f709bd0a231183d0721cf09447a2c3be
Jason Bahl <jasonbahl@mac.com>
2025-02-05 10:58:18 -0700

    - update string for i18n

--- src/Server/ValidationRules/DisableIntrospection.php
+++ src/Server/ValidationRules/DisableIntrospection.php
@@ -33,6 +33,6 @@ class DisableIntrospection extends \GraphQL\Validator\Rules\DisableIntrospection
 	 * Returns a helpful message when introspection is disabled and an introspection query is attempted.
 	 */
 	public static function introspectionDisabledMessage(): string {
-		return 'The query contained __schema or __type, however GraphQL introspection is not allowed for public requests by default. Public introspection can be enabled under the WPGraphQL Settings.';
+		return __( 'The query contained __schema or __type, however GraphQL introspection is not allowed for public requests by default. Public introspection can be enabled under the WPGraphQL Settings.', 'wp-graphql' );
 	}
 }
