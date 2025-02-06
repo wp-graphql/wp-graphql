3a2bd44895b2895840edf9330cc685605aa63a5a
Jason Bahl <jasonbahl@mac.com>
2025-02-06 13:32:50 -0700

    - change test name - update request to send json encoded content

--- tests/functional/ContentTypeHeaderCept.php
+++ tests/functional/ContentTypeHeaderCept.php
@@ -5,7 +5,7 @@ $I = new FunctionalTester($scenario);
 $I->wantTo('Ensure 415 status code is returned when content-type header is missing or incorrect');
 
 // Test with no content-type header
-$I->sendPOST('http://localhost/graphql', [
+$I->sendPOST('http://localhost/graphql', json_encode([
     'query' => '{posts{nodes{id}}}'
 ]);
 $I->seeResponseCodeIs(415);
@@ -19,7 +19,7 @@ $I->seeResponseContainsJson([
 
 // Test with incorrect content-type header
 $I->haveHttpHeader('Content-Type', 'text/plain');
-$I->sendPOST('http://localhost/graphql', [
+$I->sendPOST('http://localhost/graphql', json_encode([
     'query' => '{posts{nodes{id}}}'
 ]);
 $I->seeResponseCodeIs(415);
@@ -33,9 +33,9 @@ $I->seeResponseContainsJson([
 
 // Verify that application/json content-type works correctly
 $I->haveHttpHeader('Content-Type', 'application/json');
-$I->sendPOST('http://localhost/graphql', [
+$I->sendPOST('http://localhost/graphql', json_encode([
     'query' => '{posts{nodes{id}}}'
-]);
+]));
 $I->seeResponseCodeIs(200);
 
 // Verify that application/json with charset works
