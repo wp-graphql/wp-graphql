add3bd088c83b453c58f7f54e1910346d2e53985
Dovid Levine <david@axepress.dev>
2025-02-09 14:34:05 +0200

    fix: React backwards-compatibility with WP < 6.6

--- build/graphiqlAuthSwitch.asset.php
+++ build/graphiqlAuthSwitch.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '1bbe114c588ffebb9cde');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1c697254ccd0d5209f22');
--- packages/wpgraphiql/app.js
+++ packages/wpgraphiql/app.js
@@ -1,8 +1,25 @@
-import { render } from "@wordpress/element";
+import { createRoot, render } from "@wordpress/element";
 import "./app.scss";
 import AppWithContext from "./components/App/App.js";
 
 /**
  * Render the application to the DOM
  */
-render(<AppWithContext />, document.getElementById(`graphiql`));
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('graphiql');
+
+    if ( ! container ) {
+      return;
+  }
+
+  /**
+   * createRoot only exists in WordPress 6.2+.
+   *
+   * Once that version is the minimum required, this check can be removed.
+   */
+  if( createRoot ) {
+    createRoot( container ).render( <AppWithContext /> );
+  } else {
+    render( <AppWithContext />, container );
+  }
+});
