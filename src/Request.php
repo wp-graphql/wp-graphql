ab53c9258ce31142a633406039938593a02cd7d9
Jason Bahl <jasonbahl@mac.com>
2025-03-28 20:18:31 -0600

    - fix since next-version placeholder that didn't properly get replaced during the 2.1.0 release

--- src/Request.php
+++ src/Request.php
@@ -730,7 +730,7 @@ class Request {
 		 * @param bool $is_valid Whether the content type is valid
 		 * @param string $content_type The content type header value that was received
 		 *
-		 * @since next-version
+		 * @since 2.1.0
 		 */
 		return (bool) apply_filters( 'graphql_is_valid_http_content_type', $is_valid, $content_type );
 	}
