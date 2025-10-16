dad2de5ddc8464cf890de761cce8cddd59086d1d
Jason Bahl <jasonbahl@mac.com>
2025-10-16 10:51:57 -0600

    Clean up config.php by removing debug settings
    Removed unnecessary debug and database configuration settings.

--- tests/_data/config.php
+++ tests/_data/config.php
@@ -8,11 +8,3 @@ define( 'GRAPHQL_DEBUG', true );
 define( 'WPGRAPHQL_AUTOLOAD', false );
 define( 'WP_AUTO_UPDATE_CORE', false );
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
-
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', false );
-define( 'DB_NAME', 'wp_graphql_test' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', '' );
-define( 'DB_HOST', '127.0.0.1' );
