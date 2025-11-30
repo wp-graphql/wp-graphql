62098997e4aea7d3453999c1aa7ace244a4239d2
Dovid Levine <david@axepress.dev>
2025-11-30 17:59:23 +0200

    ci: exit command on failed `pdo_mysql`

--- bin/setup-wp-env.sh
+++ bin/setup-wp-env.sh
@@ -10,18 +10,20 @@ npm run wp-env run tests-cli  -- wp rewrite flush --hard
 # Get install Path
 cd $(wp-env install-path)
 
-# Reload Apache flag
-RELOAD=false
 
 # Install pdo on tests-cli
 if [[ $(docker compose exec -T -u root tests-cli php -m | grep pdo_mysql) != "pdo_mysql" ]]; then
 	echo "Installing: pdo_mysql Extension on tests-cli."
-	docker compose exec -T -u root tests-cli docker-php-ext-install pdo_mysql
-
-	if [[ $(docker compose exec -T -u root tests-cli php -m | grep pdo_mysql) == "pdo_mysql" ]]; then
-		echo "pdo_mysql Extension on tests-cli: Installed."
+	if docker compose exec -T -u root tests-cli docker-php-ext-install pdo_mysql; then
+		if [[ $(docker compose exec -T -u root tests-cli php -m | grep pdo_mysql) == "pdo_mysql" ]]; then
+			echo "pdo_mysql Extension on tests-cli: Installed."
+		else
+			echo "ERROR: pdo_mysql Extension on tests-cli: Installation command succeeded but extension not loaded." >&2
+			exit 1
+		fi
 	else
-		echo "pdo_mysql Extension on tests-cli: Failed."
+		echo "ERROR: pdo_mysql Extension on tests-cli: Installation failed." >&2
+		exit 1
 	fi
 else
 	echo "pdo_mysql Extension on tests-cli: Already installed."
