d23a9fe28f6a34583dc57d204557a4dfa3af0e59
Jason Bahl <jasonbahl@mac.com>
2025-12-17 06:05:47 +0000

    chore: simplify bootstrap files
    Remove redundant maintenance mode filters - now handled by wp-config constants

--- tests/functional/bootstrap.php
+++ tests/functional/bootstrap.php
@@ -6,15 +6,7 @@
  */
 
 /**
- * Disable updates to prevent WP from going into maintenance mode while tests run.
- */
-add_filter( 'enable_maintenance_mode', '__return_false' );
-add_filter( 'wp_auto_update_core', '__return_false' );
-add_filter( 'auto_update_plugin', '__return_false' );
-add_filter( 'auto_update_theme', '__return_false' );
-
-/**
- * Helper constant used by the tests to query the GraphQL sites.
+ * Helper constant used by the tests to query the GraphQL endpoint.
  */
 if ( ! defined( 'TEST_GRAPHQL_ENDPOINT' ) ) {
 	$graphql_base = getenv( 'TEST_WP_URL' );
