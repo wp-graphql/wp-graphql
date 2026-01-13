6462c2922be7584ef1022801fa7f378b5f89e7c5
Jason Bahl <jasonbahl@mac.com>
2026-01-13 19:21:59 +0000

    feat: add release-please configuration and workflow
    - Add release-please-config.json for wp-graphql package
    - Add .release-please-manifest.json to track current version
    - Add release-please.yml workflow with WordPress.org deployment
    - Add breaking change marker validation to lint-pr.yml
    - Add x-release-please-version annotations to version files
    
    This sets up release-please for automated releases using squash merge
    workflow with PR title validation.
    
    Ref: https://github.com/wp-graphql/wp-graphql/issues/3469

--- plugins/wp-graphql/wp-graphql.php
+++ plugins/wp-graphql/wp-graphql.php
@@ -7,7 +7,7 @@
  * Description: GraphQL API for WordPress
  * Author: WPGraphQL
  * Author URI: http://www.wpgraphql.com
- * Version: 2.6.0
+ * Version: 2.6.0 x-release-please-version
  * Text Domain: wp-graphql
  * Domain Path: /languages/
  * Requires at least: 6.0
@@ -19,7 +19,7 @@
  * @package  WPGraphQL
  * @category Core
  * @author   WPGraphQL
- * @version  2.6.0
+ * @version  2.6.0 x-release-please-version
  */
 
 // Exit if accessed directly.
