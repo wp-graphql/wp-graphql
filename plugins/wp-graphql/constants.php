b75422659fdc8fb17729bed905acbee798fb6d25
Jason Bahl <jasonbahl@mac.com>
2026-01-23 14:24:51 -0700

    chore: update source files to version 2.7.0
    - Update Version in wp-graphql.php to 2.7.0
    - Update WPGRAPHQL_VERSION in constants.php to 2.7.0
    - Set readme.txt Stable tag back to x-release-please-version placeholder for future releases

--- plugins/wp-graphql/constants.php
+++ plugins/wp-graphql/constants.php
@@ -18,7 +18,7 @@ function graphql_setup_constants() {
 
 	// Plugin version.
 	if ( ! defined( 'WPGRAPHQL_VERSION' ) ) {
-		define( 'WPGRAPHQL_VERSION', '2.6.0' );
+		define( 'WPGRAPHQL_VERSION', '2.7.0' );
 	}
 
 	// Plugin Folder Path.
