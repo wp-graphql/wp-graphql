0a5660e077f4c660933215803a85465b19b2b322
Jason Bahl <jasonbahl@mac.com>
2026-01-26 11:32:40 -0700

    fix: add JSON headers to QueryIdCest acceptance tests
    - Add Content-Type: application/json header before POST requests
    - Use sendPOST() with json_encode() instead of sendPost() with array
    - Fix error structure to expect errors[0]['message'] array format
    
    Fixes acceptance test failures where POST requests were sent as
    application/x-www-form-urlencoded instead of application/json.

--- plugins/wp-graphql-smart-cache/tests/acceptance/QueryIdCest.php
+++ plugins/wp-graphql-smart-cache/tests/acceptance/QueryIdCest.php
@@ -15,30 +15,35 @@ class QueryIdCest
 	// If send empty string and a query id that does not exist, will get not found error
 	public function sendEmptyQueryStringWithQueryIdIsErrorTest(AcceptanceTester $I)
 	{
-		$I->sendPost('graphql', [
+		$I->haveHttpHeader('Content-Type', 'application/json');
+		$I->sendPOST('graphql', json_encode([
 			'query' => '',
 			'queryId' => 'alias-does-not-exist'
-		] );
+		]));
 		$I->seeResponseContainsJson([
 			'errors' => [
-				'message' => 'PersistedQueryNotFound'
+				[
+					'message' => 'PersistedQueryNotFound'
+				]
 			]
 		]);
 	}
 
 	public function saveQueryAndHashTest(AcceptanceTester $I)
 	{
-		$I->sendPost('graphql', [
+		$I->haveHttpHeader('Content-Type', 'application/json');
+		$I->sendPOST('graphql', json_encode([
 			'query' => '{__typename}',
 			'queryId' => '8d8f7365e9e86fa8e3313fcaf2131b801eafe9549de22373089cf27511858b39'
-		] );
+		]));
 		$I->seeResponseContainsJson([
 		   'data' => [
 			   '__typename' => 'RootQuery'
 		   ]
 		]);
 
-		$I->sendPost('graphql', [
+		$I->haveHttpHeader('Content-Type', 'application/json');
+		$I->sendPOST('graphql', json_encode([
 			'query' => '{ __typename }',
 			'extensions' => [
 				"persistedQuery" => [
@@ -46,7 +51,7 @@ class QueryIdCest
 					"sha256Hash" => "8d8f7365e9e86fa8e3313fcaf2131b801eafe9549de22373089cf27511858b39"
 				]
 			]
-		] );
+		]));
 		$I->seeResponseContainsJson([
 			'data' => [
 				'__typename' => 'RootQuery'
