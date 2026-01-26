2ece498a80f86d9a09889a15264926e4a99a38d2
Jason Bahl <jasonbahl@mac.com>
2026-01-26 12:23:41 -0700

    refactor: centralize test bootstrap files to reduce duplication
    - Create shared bootstrap-common.php in wp-graphql/tests
    - Update all plugin bootstrap files to require the common bootstrap
    - Reduces duplication of TEST_GRAPHQL_ENDPOINT definition across plugins
    - Makes it easier to add shared test utilities in the future

--- plugins/wp-graphql-smart-cache/tests/acceptance/bootstrap.php
+++ plugins/wp-graphql-smart-cache/tests/acceptance/bootstrap.php
@@ -5,10 +5,8 @@
  * @package WPGraphQL\SmartCache\Tests\Acceptance
  */
 
-/**
- * Helper constant used by the tests to query the GraphQL endpoint.
- */
-if ( ! defined( 'TEST_GRAPHQL_ENDPOINT' ) ) {
-	$graphql_base = getenv( 'TEST_WP_URL' );
-	define( 'TEST_GRAPHQL_ENDPOINT', rtrim( $graphql_base, '/' ) . '/graphql' );
-}
+// Load common bootstrap from wp-graphql plugin (shared across monorepo)
+// Path: plugins/wp-graphql-smart-cache/tests/acceptance -> plugins/wp-graphql/tests
+// From: plugins/wp-graphql-smart-cache/tests/acceptance
+// To: plugins/wp-graphql/tests/bootstrap-common.php
+require_once dirname( dirname( dirname( __DIR__ ) ) ) . '/wp-graphql/tests/bootstrap-common.php';
