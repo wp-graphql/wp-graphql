a6dd4bb44ba724ba3efe45fa9a39d93eee945944
Jason Bahl <jasonbahl@mac.com>
2026-04-03 12:11:24 -0600

    chore(pqc): align PHPStan config and add analysis stubs
    Bootstrap constants for analysis, scan WP-CLI stubs, allow stdClass
    response crates, and stub WP_CLI\\Utils\\make_progress_bar.

---
+++ plugins/wp-graphql-pqc/phpstan/constants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Constants for PHPStan analysis (WordPress / dependent plugins not loaded).
+ *
+ * @package WPGraphQL\PQC
+ */
+
+if ( ! defined( 'PHPSTAN' ) ) {
+	define( 'PHPSTAN', true );
+}
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __DIR__ ) . '/' );
+}
+
+// Satisfy dependency checks referenced by App::can_load_plugin().
+if ( ! defined( 'WPGRAPHQL_VERSION' ) ) {
+	define( 'WPGRAPHQL_VERSION', '2.0.0' );
+}
+if ( ! defined( 'WPGRAPHQL_SMART_CACHE_VERSION' ) ) {
+	define( 'WPGRAPHQL_SMART_CACHE_VERSION', '2.0.0' );
+}
