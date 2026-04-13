f82c939b88de8b08bdd5aff3baa1da4d055dafdd
Jason Bahl <jasonbahl@mac.com>
2026-04-13 14:22:57 -0600

    feat: add Appsero telemetry tracking to WPGraphQL IDE (#3765)

---
+++ plugins/wp-graphql-ide/phpstan/constants.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Constants defined in this file are to help PHPStan analyze code where
+ * constants outside the plugin (WordPress core constants, etc.) are being used.
+ */
+
+define( 'PHPSTAN', true );
